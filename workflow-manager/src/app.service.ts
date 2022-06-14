035da1ee0fefeb92864e7363fd6623ac5bbc9317
Muhannad <muh.acit@Gmail.com>
2022-06-14 03:35:56 +0300

    changing the name of event-steam-client to workflow manager

---
+++ workflow-manager/src/app.service.ts
@@ -0,0 +1,51 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class AppService {
+  constructor(@Inject('LIMITS_SERVICE') private client: ClientProxy) {}
+
+  async performRequestAction( action: string) {
+    console.log('WFM event emitted');
+    //console.info(event);
+    this.client.emit('wfm-limit-event', {
+      action: action,
+      data: {
+        rejection_reason: 'some reason',
+        operation: 'update',
+        fields: [
+          {
+            field_name: 'some_name_1',
+            field_value: 'some value 1',
+          },
+          {
+            field_name: 'some_name_2',
+            field_value: 'some value 2',
+          },
+        ],
+      },
+    });
+  }
+
+  async performCharityRequestAction( action: string) {
+    console.log('WFM event emitted');
+    //console.info(event);
+    this.client.emit('wfm-charity-event', {
+      action: action,
+      data: {
+        rejection_reason: 'some reason',
+        operation: 'update',
+        fields: [
+          {
+            field_name: 'some_name_1',
+            field_value: 'some value 1',
+          },
+          {
+            field_name: 'some_name_2',
+            field_value: 'some value 2',
+          },
+        ],
+      },
+    });
+  }
+}
